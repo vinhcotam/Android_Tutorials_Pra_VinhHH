8372e35f912291cedc6845a5ae78dd94e3d4e29e
vinhcotam <66359687+vinhcotam@users.noreply.github.com>
2022-12-26 16:38:08 +0700

    first

---
+++ app/src/main/java/com/sun/android/ex_slide9/DroidCafe.kt
@@ -0,0 +1,11 @@
+package com.sun.android.ex_slide9
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DroidCafe : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_droid_cafe)
+    }
+}
